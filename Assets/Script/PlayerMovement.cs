0977accf3aa421bd8e664810d4dd343b7f869333
WasutepPetch <killermlg413@gmail.com>
2026-09-17 05:21:56 +0700

    removed Input packages
    It's a packages made for newer Input system which we will not touch so i thought it's unnecessary to keep it around

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,23 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed; 
-    private Vector2 moveInput;
+    public float speed;
     public Rigidbody2D rb;
     public void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    void FixedUpdate()
+    public void FixedUpdate()
     {
-        rb.linearVelocity = moveInput * speed;
-    }
-    public void OnMove(InputValue inputValue)
-    {
-        moveInput = inputValue.Get<Vector2>();
+
     }
 }
