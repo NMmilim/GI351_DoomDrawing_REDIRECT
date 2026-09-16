bafa687f5b47ce14eca6229cc98797b820a8f1c6
WasutepPetch <killermlg413@gmail.com>
2026-09-16 21:21:57 +0700

    Player script
    Movement
    Player empty script

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody2D rb;
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+}
