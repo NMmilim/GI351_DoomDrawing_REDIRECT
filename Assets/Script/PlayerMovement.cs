d4622d28c86b0ef8eaeb8aaf267a3e717473a462
WasutepPetch <killermlg413@gmail.com>
2026-09-17 05:26:59 +0700

    PlayerMovgement
    WASD to move
    W go upward
    A go left
    S go down
    D go right
    with speed shown in inspector for manually changing

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -13,6 +13,10 @@ public class PlayerMovement : MonoBehaviour
     }
     public void FixedUpdate()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
 
     }
 }
