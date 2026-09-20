af4298142d2eab92801d7fc2e1f50ca80b02b9b5
WasutepPetch <killermlg413@gmail.com>
2026-09-20 23:05:51 +0700

    hide enemy view chasing bool

--- Assets/Script/Enemscript/melee_enem.cs
+++ Assets/Script/Enemscript/melee_enem.cs
@@ -18,12 +18,12 @@ public class melee_enem : Enemy
     }
     void Update()
     {
-        if (enemView.IsChasing == true)
-        {
-            Vector2 direction = (player.position - transform.position).normalized;
-            transform.position += (Vector3)direction * movespeed * Time.deltaTime;
-            transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90);
-        }
+       //// if (enemView.IsChasing == true)
+       // {
+       //     Vector2 direction = (player.position - transform.position).normalized;
+       //     transform.position += (Vector3)direction * movespeed * Time.deltaTime;
+       //     transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90);
+       // }
 
 
 
